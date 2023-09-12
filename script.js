2581c3bb287d238d1dca25819e5f978a2dc4c27f
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-09-12 22:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let mp = {};
+let touristSport = ['The Virupaksha Temple', 'Vivtoria memorial', 'tajmahal'];
 
+let articleLArray=[];
+for articleLessIteam = [];
+for(let i=0; i<touristSports[i].replace(/\bthe\b|\ban\b|\ba\b/gi, " ").trim();
+	{
+		articleLessArraay.push(articleLessIteam);
+     mp[articleLessIteam] = touristSpots[i];
+	}
+
+    articleLessArray.sort();
+   let ans = [];
+   for(let i of articleLessArray){
+	   ans.push(mp[i]);
+   }
+console.log(ans);
